9b3066bdcd0cba7c912b3f7bb4dea455ce37f1bb
Ntombikayise <Entleh6761@gmail.com>
2023-08-03 17:01:35 -0700

    wuu

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,31 +1,30 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_left - faka i node njenge ngane yakwesokunxele kwenye i node
- * @parent: pointer kwi node yokufaka ingane yakwesokunxele phakathi
- * @value: ivelu yokubekwa kwinode entsha
- *
- * Return: Pointer to the newly created node
- *         NULL on failure
- *         NULL if parent is NULL
+ * binary_tree_insert_left - add a node in the left of the parent
+ * uma i[hila izokwehlela phansi one level and add the new node first
+ * @parent: umzali we specified node
+ * @value: ivelu ye node
+ * Return: NULL if it fails or the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new_node;
 
-	if (!parent)
+	if (parent == NULL)
+	{
 		return (NULL);
+	}
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
 		return (NULL);
-
-	new->n = value;
-	new->parent = parent;
-	new->right = NULL;
-	new->left = parent->left;
-	parent->left = new;
-	if (new->left)
-		new->left->parent = new;
-	return (new);
+	}
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,20 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_postorder - Traverses a binary tree isebenzisa post-order traversal.
- * @tree: A pointer yakwi mpande ye node ye tree to traverse.
- * @func: A pointer kwi fankshini ukubiza for each node.
+ * binary_tree_postorder - print elements of tree using post-order traversal
+ * @tree: tree okuzodlulwa kuyo
+ * @func: ifankshini ezosebenza
+ * Return: Nothing
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
 	{
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
-		func(tree->n);
 	}
+	func(tree->n);
 }
